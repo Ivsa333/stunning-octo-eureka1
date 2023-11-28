2895d5d82fa12618873fb2d7683f9f629f57ef54
Iván Borraz García <ivanborrazgarcia@gmail.com>
2023-11-28 11:27:18 +0100

    añadiendo mi nombre y nombre del instituto

--- App.java
+++ App.java
@@ -1,9 +1,12 @@
 public class App {
   
   public static void main(String[] args) {
+    System.out.println("Mi nombre es Ivan Borraz");
     System.out.println("Contenido 2a");
     System.out.println("Contenido 3");
     System.out.println("Contenido 2b");
+    System.out.println("IES San Andres del Rabanedo");
+    System.out.println("Vivo en Eras de Renueva");
   }
   
 }
